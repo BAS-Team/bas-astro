f16c4dccfc979607425be71ee531fd8532758dc9
mzkki <08harismu@gmail.com>
2024-02-06 12:08:49 +0800

    update: styling for footer

--- src/navigation.js
+++ src/navigation.js
@@ -8,23 +8,27 @@ export const headerData = {
     },
     {
       text: 'About',
-      href: '#about',
+      href: '/#about',
     },
     {
       text: 'Services',
-      href: '#services',
+      href: '/#services',
     },
     {
       text: 'Portfolio',
-      href: '#portfolio',
+      href: '/#portfolio',
     },
     {
       text: 'Blog',
       href: getBlogPermalink('/'),
     },
     {
-      text: 'Widgets',
-      href: '#',
+      text: 'Contact',
+      href: '/#contact',
+    },
+    {
+      text: 'Training',
+      href: getPermalink('/training'),
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
@@ -89,7 +93,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> Bina Anugerah Sukses</a> · All rights reserved.
   `,
 };
